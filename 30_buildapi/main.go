3f54c3467d14b0bf7fb075c67d6f03e9c05aafa3
AnilKumsrsingh <anilkumarsingh5698@gmail.com>
2026-01-04 17:12:31 +0000

    learn to handle http reqeust

--- 30_buildapi/main.go
+++ 30_buildapi/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
 	"strconv"
-	"math/rand"
 
 	"github.com/gorilla/mux"
 )
@@ -29,10 +30,35 @@ var coursesDB []Course
 // middleware, helper - file
 
 func (c *Course) IsEmpty() bool {
-	return  c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
 }
 
 func main() {
+
+	fmt.Println("API - LearnCodeOnline.in")
+	r := mux.NewRouter()
+
+	// seeding
+	coursesDB = append(coursesDB, Course{CourseId: "2" , CourseName: "Golang", CoursePrice: 244, 
+	Author: &Author{FullName: "Anil Kumar Singh", Website: "hitech.com"}})	
+
+	coursesDB = append(coursesDB, Course{CourseId: "3" , CourseName: "Rust", CoursePrice: 300, 
+	Author: &Author{FullName: "Anil Kumar Singh", Website: "hitech.com"}})	
+
+	// routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", addOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
+	
+
+
+
+
+	// listen to a port
+	log.Fatal(http.ListenAndServe(":4000", r))
 
 }
 
@@ -89,6 +115,14 @@ func addOneCourse(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
+	// TODO: LOOP, REMOVE DUPLICATE COURSE
+	for _, old_course := range coursesDB{
+		if old_course.CourseName == course.CourseName{
+			json.NewEncoder(w).Encode("Cannot add same course twice")
+			return
+		}
+	}
+
 	// generate unique id, string
 	// append course into courseDB
 
@@ -96,4 +130,46 @@ func addOneCourse(w http.ResponseWriter, r *http.Request){
 	coursesDB = append(coursesDB, course)
 	json.NewEncoder(w).Encode(course)
 	return
+}
+
+
+// update a course controller in golang
+func updateOneCourse (w http.ResponseWriter, r *http.Request){
+	fmt.Println("Updating one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	// loop, id, remove, add new content with same id
+
+	for index, course := range coursesDB{
+		if course.CourseId == params["id"]{
+			coursesDB = append(coursesDB[:index], coursesDB[index+1:]...)
+			var newCourse Course
+			_ = json.NewDecoder(r.Body).Decode(&newCourse)
+			newCourse.CourseId = params["id"]
+			coursesDB = append(coursesDB, newCourse)
+			json.NewEncoder(w).Encode(newCourse)
+			return
+		}
+	}
+	// TODO: send a response when id is not found
+}
+
+
+// delete a course controller in golang
+func deleteOneCourse (w http.ResponseWriter, r *http.Request){
+	fmt.Println("deleting one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	// loop, id, remove (index, index+1)
+
+	for index, course := range coursesDB{
+		if course.CourseId == params["id"]{
+			coursesDB = append(coursesDB[:index], coursesDB[index+1:]...)
+			return
+		}
+	}
 }
